037429e62dea5e367c706bea0aeb18ffbb077a0f
HP <winone666@yandex.ru>
2020-12-14 00:14:13 +0700

    Add module_5 page object model test

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -9,6 +10,8 @@ class TestMainPage:
         page = MainPage(browser, link)
         page.open()
         page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
         page = MainPage(browser, link)
